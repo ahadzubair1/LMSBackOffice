8576429095413ebc2ca5a866b67bee60e36cc4c0
reshma_sree <reshma.mohan910@gmail.com>
2024-03-07 15:58:21 +0400

    git commits

--- LMSBackOfficeGateway/obj/Debug/net6.0/LMSBackOfficeGateway.AssemblyInfo.cs
+++ LMSBackOfficeGateway/obj/Debug/net6.0/LMSBackOfficeGateway.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LMSBackOfficeGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a029df43c8074725f9f79ffe7dd00cdbbfc4d122")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("LMSBackOfficeGateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LMSBackOfficeGateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
