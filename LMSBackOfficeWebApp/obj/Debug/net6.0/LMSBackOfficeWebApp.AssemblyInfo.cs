24846e191299b3d2de41fde57ec4cb2c087a33f0
reshma_sree <reshma.mohan910@gmail.com>
2024-03-07 15:58:38 +0400

    git commits

--- LMSBackOfficeWebApp/obj/Debug/net6.0/LMSBackOfficeWebApp.AssemblyInfo.cs
+++ LMSBackOfficeWebApp/obj/Debug/net6.0/LMSBackOfficeWebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LMSBackOfficeWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7ca7a2440292da611b5d4de93a86393322eea99")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("LMSBackOfficeWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LMSBackOfficeWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
