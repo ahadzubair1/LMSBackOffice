78d61719d50144f1d99180f94add74e9e373ca60
Salman Ahmad <salman@vmarketing.com>
2024-03-21 12:20:30 +0400

    Updates

--- LMSBackOfficeWebApplication/Networks.aspx.cs
+++ LMSBackOfficeWebApplication/Networks.aspx.cs
@@ -1,5 +1,4 @@
-﻿using LMSBackOfficeDAL;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
@@ -7,12 +6,15 @@ using System.Text;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using LMSBackOfficeDAL; // Make sure to include the necessary namespace
 
 namespace LMSBackOfficeWebApplication
 {
     public partial class Networks : System.Web.UI.Page
     {
         protected DataTable referrelsTable { get; set; }
+        protected DataTable networkTreeTable { get; set; }
+
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
@@ -25,108 +27,259 @@ namespace LMSBackOfficeWebApplication
                 string userName = Session["Username"].ToString();
                 var member = Members_DataAccess.GetMemberInfo(userName);
                 referrelsTable = Members_DataAccess.GetReferrelsByMemberId(member.Id);
+                networkTreeTable = NetworkTree_DataAccess.GetNetworkTree(member.Id);
 
-            }
-
-            if (!IsPostBack)
-            {
-                string generatedHtml = GenerateHTML(); // Call the method to generate HTML
-                litGeneratedHtml.Text = generatedHtml; // Set the generated HTML to the Literal control
+                // Generate HTML only once during the initial load
+                string generatedHtml = GenerateHTML(networkTreeTable);
+                litGeneratedHtml.Text = generatedHtml;
             }
         }
-        private string GenerateHTML()
+
+        private string GenerateHTML(DataTable dataTable)
         {
             StringBuilder sb = new StringBuilder();
 
-            // Appending HTML structure
+
+            string userName = Session["Username"].ToString();
+            var member = Members_DataAccess.GetMemberInfo(userName);
+            // Get root nodes (Position = 0)
+            DataRow[] rootNodes = dataTable.Select($"Member_Id = '{member.Id}'");
+
+            // Start the parent node
             sb.AppendLine("<ul class=\"parent-node\">");
-            sb.AppendLine("    <li>");
-            sb.Append(GenerateUserHtml("Anas AORH7", "EWGRR", "Morocco", "Platinum", "02/02/2024 23:59:02"));
+            //sb.AppendLine("    <li>");
+
+
+            string memberUserName = rootNodes[0]["Member_UserName"].ToString();
+            string position = rootNodes[0]["Position"].ToString();
+            string country = rootNodes[0]["CountryOfOrigin"].ToString(); // Add country data
+
+            // Start the list item for the current node
+            sb.AppendLine("<li>");
+            // Generate HTML for the current node
+            sb.AppendLine("<a href=\"#\" class=\"platinum\">");
+            sb.AppendLine($@"
+                        <img class=""user-rank"" src=""Content/images/user/avatar-2.jpg"" data-toggle=""tooltip"" data-placement=""top"" title=""Platinum"">
+                        <img class=""user-avatar"" src=""Content/images/user/avatar-2.jpg"">
+                        <span class=""user-name"">{memberUserName}</span>
+                        <span class=""node-detail"">
+                            <label>Username: {memberUserName}</label>
+                            <label>Position: {position}</label>
+                            <label>Country: {country}</label>
+                        </span>
+                    </a>");
+
+
+
             sb.AppendLine("        <ul class=\"child-node\">");
-            sb.AppendLine("            <li>");
-            sb.Append(GenerateUserHtml("Ahmed AEPNJ", "AORH7", "Egypt", "Platinum", "02/02/2024 23:59:02"));
-            sb.AppendLine("                <ul>");
-            sb.AppendLine("                    <li>");
-            sb.Append(GenerateUserHtml("Omar MDTA4", "AORH7", "Morocco", "Premium", "02/03/2024 01:44:12"));
-            sb.AppendLine("                    <ul>");
-            sb.AppendLine("                        <li>");
-            sb.Append(GenerateUserHtml("ANAS OK7PM", "AORH7", "Morocco", "Starter", "02/03/2024 02:05:12"));
-            sb.AppendLine("                        </li>");
-            sb.AppendLine("                        <li>");
-            sb.Append(GenerateUserHtml("bougouttaia HDWGT", "MDTA4", "Morocco", "Starter", "02/03/2024 02:05:12"));
-            sb.AppendLine("                        </li>");
-            sb.AppendLine("                    </ul>");
-            sb.AppendLine("                </li>");
-            sb.AppendLine("                <li>");
-            sb.Append(GenerateUserHtml("No User", "", "AE", "NA", "02/03/2024 01:44:12"));
-            sb.AppendLine("                    <ul>");
-            sb.AppendLine("                        <li>");
-            sb.Append(GenerateUserHtml("No User", "", "AE", "NA", ""));
-            sb.AppendLine("                        </li>");
-            sb.AppendLine("                        <li>");
-            sb.Append(GenerateUserHtml("No User", "", "AE", "NA", ""));
-            sb.AppendLine("                        </li>");
-            sb.AppendLine("                    </ul>");
-            sb.AppendLine("                </li>");
-            sb.AppendLine("            </ul>");
-            sb.AppendLine("        </li>");
-            sb.AppendLine("        <li>");
-            sb.Append(GenerateUserHtml("ANAS 5YABF", "AORH7", "Morocco", "Platinum", "02/02/2024 23:59:02"));
-            sb.AppendLine("            <ul>");
-            sb.AppendLine("                <li>");
-            sb.Append(GenerateUserHtml("No User", "", "AE", "NA", "02/03/2024 00:27:21"));
-            sb.AppendLine("                    <ul>");
-            sb.AppendLine("                        <li>");
-            sb.Append(GenerateUserHtml("No User", "", "AE", "NA", ""));
-            sb.AppendLine("                        </li>");
-            sb.AppendLine("                        <li>");
-            sb.Append(GenerateUserHtml("No User", "", "AE", "NA", ""));
-            sb.AppendLine("                        </li>");
-            sb.AppendLine("                    </ul>");
-            sb.AppendLine("                </li>");
-            sb.AppendLine("                <li>");
-            sb.Append(GenerateUserHtml("Abdessamad A7AFX", "AORH7", "Netherlands", "Starter", "02/03/2024 00:27:21"));
-            sb.AppendLine("                    <ul>");
-            sb.AppendLine("                        <li>");
-            sb.Append(GenerateUserHtml("No User", "", "AE", "NA", "02/26/2024 02:29:08"));
-            sb.AppendLine("                        </li>");
-            sb.AppendLine("                        <li>");
-            sb.Append(GenerateUserHtml("ANMAR 5NICM", "AORH7", "Iraq", "Starter", "02/26/2024 02:29:08"));
-            sb.AppendLine("                        </li>");
-            sb.AppendLine("                    </ul>");
-            sb.AppendLine("                </li>");
-            sb.AppendLine("            </ul>");
-            sb.AppendLine("        </li>");
+
+            foreach (DataRow rootNode in rootNodes)
+            {
+                string memberId = rootNode["Member_ID"].ToString();
+                // Generate HTML for the root node and its children recursively
+                sb.Append(GenerateNodeHtml(dataTable, memberId));
+            }
             sb.AppendLine("    </ul>");
             sb.AppendLine("</li>");
             sb.AppendLine("</ul>");
 
+
             return sb.ToString();
         }
 
-
-        public string GenerateUserHtml(string userName, string sponsor, string country, string spotLicense, string subsDate)
+        private string GenerateNodeHtmlTest(DataTable dataTable, string parentId)
         {
             StringBuilder sb = new StringBuilder();
 
-            sb.AppendLine("<a href=\"\" class=\"platinum\"\">");
-            sb.AppendLine($@"
-            
-                <img class=""user-rank"" src=""Content/images/user/avatar-2.jpg"" data-toggle=""tooltip"" data-placement=""top"" title=""Gold"">
-                <img class=""user-avatar"" src=""Content/images/user/avatar-2.jpg"">
-                <span class=""user-name"">{userName}</span>
-                <span class=""node-detail"">
-                    <label>Username: {userName}</label>
-                    <label>Sponsor: {sponsor}</label>
-                    <label>Country: {country}</label>
-                    <label>Spot License: {spotLicense}</label>
-                    <label>Subs. Date: {subsDate}</label>
-                </span>
-            </a>
-        ");
+            // Get the current node and its children
+            DataRow[] childRows = dataTable.Select($"Parent_MemberId = '{parentId}'");
+            if (childRows.Length > 0)
+            {
+                foreach (DataRow row in childRows)
+                {
+                    string memberUserName = row["Member_UserName"].ToString();
+                    string position = row["Position"].ToString();
+                    string country = row["CountryOfOrigin"].ToString(); // Add country data
+
+                    // Start the list item for the current node
+                    sb.AppendLine("<li>");
+                    // Generate HTML for the current node
+                    sb.AppendLine("<a href=\"#\" class=\"platinum\">");
+                    sb.AppendLine($@"
+                        <img class=""user-rank"" src=""Content/images/user/avatar-2.jpg"" data-toggle=""tooltip"" data-placement=""top"" title=""Platinum"">
+                        <img class=""user-avatar"" src=""Content/images/user/avatar-2.jpg"">
+                        <span class=""user-name"">{memberUserName}</span>
+                        <span class=""node-detail"">
+                            <label>Username: {memberUserName}</label>
+                            <label>Position: {position}</label>
+                            <label>Country: {country}</label>
+                        </span>
+                    </a>");
+                    // Check if there are child nodes for the current node
+                    string memberId = row["Member_ID"].ToString();
+                    string childHtml = GenerateNodeHtml(dataTable, memberId);
+                    if (!string.IsNullOrEmpty(childHtml))
+                    {
+                        // Start the child node
+                        sb.AppendLine("<ul class=\"child-node\">");
+                        sb.AppendLine(childHtml);
+                        // End the child node
+                        sb.AppendLine("</ul>");
+
+                    }
+                    // End the list item for the current node
+                    sb.AppendLine("</li>");
+                }
+
+            }
 
             return sb.ToString();
         }
 
+
+        private string GenerateNodeHtml(DataTable dataTable, string parentId)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            // Get the current node and its children
+            DataRow[] leftChildRows = dataTable.Select($"Parent_MemberId = '{parentId}' and Position=1");
+            if (leftChildRows.Length > 0)
+            {
+
+
+                string memberUserName = leftChildRows[0]["Member_UserName"].ToString();
+                string position = leftChildRows[0]["Position"].ToString();
+                string country = leftChildRows[0]["CountryOfOrigin"].ToString(); // Add country data
+
+                // Start the list item for the current node
+                sb.AppendLine("<li>");
+                // Generate HTML for the current node
+                sb.AppendLine("<a href=\"#\" class=\"platinum\">");
+                sb.AppendLine($@"
+                        <img class=""user-rank"" src=""Content/images/user/avatar-2.jpg"" data-toggle=""tooltip"" data-placement=""top"" title=""Platinum"">
+                        <img class=""user-avatar"" src=""Content/images/user/avatar-2.jpg"">
+                        <span class=""user-name"">{memberUserName}</span>
+                        <span class=""node-detail"">
+                            <label>Username: {memberUserName}</label>
+                            <label>Position: {position}</label>
+                            <label>Country: {country}</label>
+                        </span>
+                    </a>");
+                // Check if there are child nodes for the current node
+                string memberId = leftChildRows[0]["Member_ID"].ToString();
+                if(Int32.Parse(leftChildRows[0]["Level"].ToString())<=3)
+                {
+                    string childHtml = GenerateNodeHtml(dataTable, memberId);
+                    if (!string.IsNullOrEmpty(childHtml))
+                    {
+                        // Start the child node
+                        sb.AppendLine("<ul class=\"child-node\">");
+                        sb.AppendLine(childHtml);
+                        // End the child node
+                        sb.AppendLine("</ul>");
+
+                    }
+                }
+                
+                // End the list item for the current node
+                sb.AppendLine("</li>");
+
+
+            }
+            else
+            {
+
+                // Start the list item for the current node
+                sb.AppendLine("<li>");
+                // Generate HTML for the current node
+                sb.AppendLine("<a href=\"#\" class=\"platinum\">");
+                sb.AppendLine($@"
+                        <img class=""user-rank"" src=""Content/images/user/avatar-2.jpg"" data-toggle=""tooltip"" data-placement=""top"" title=""Platinum"">
+                        <img class=""user-avatar"" src=""Content/images/user/avatar-2.jpg"">
+                        <span class=""user-name"">No Member</span>
+                        <span class=""node-detail"">
+                            <label>Username: No User</label>
+                            <label>Position: NA</label>
+                            <label>Country: NA</label>
+                        </span>
+                    </a>");
+ 
+                
+                // End the list item for the current node
+                sb.AppendLine("</li>");
+
+
+            }
+
+            DataRow[] RightChildRows = dataTable.Select($"Parent_MemberId = '{parentId}' and Position=2");
+            if (RightChildRows.Length > 0)
+            {
+
+
+                string memberUserName = RightChildRows[0]["Member_UserName"].ToString();
+                string position = RightChildRows[0]["Position"].ToString();
+                string country = RightChildRows[0]["CountryOfOrigin"].ToString(); // Add country data
+
+                // Start the list item for the current node
+                sb.AppendLine("<li>");
+                // Generate HTML for the current node
+                sb.AppendLine("<a href=\"#\" class=\"platinum\">");
+                sb.AppendLine($@"
+                        <img class=""user-rank"" src=""Content/images/user/avatar-2.jpg"" data-toggle=""tooltip"" data-placement=""top"" title=""Platinum"">
+                        <img class=""user-avatar"" src=""Content/images/user/avatar-2.jpg"">
+                        <span class=""user-name"">{memberUserName}</span>
+                        <span class=""node-detail"">
+                            <label>Username: {memberUserName}</label>
+                            <label>Position: {position}</label>
+                            <label>Country: {country}</label>
+                        </span>
+                    </a>");
+                // Check if there are child nodes for the current node
+                string memberId = RightChildRows[0]["Member_ID"].ToString();
+                if (Int32.Parse(RightChildRows[0]["Level"].ToString()) <= 3)
+                {
+                    string childHtml = GenerateNodeHtml(dataTable, memberId);
+                    if (!string.IsNullOrEmpty(childHtml))
+                    {
+                        // Start the child node
+                        sb.AppendLine("<ul class=\"child-node\">");
+                        sb.AppendLine(childHtml);
+                        // End the child node
+                        sb.AppendLine("</ul>");
+
+                    }
+                }
+                // End the list item for the current node
+                sb.AppendLine("</li>");
+
+
+            }
+            else
+            {
+
+                // Start the list item for the current node
+                sb.AppendLine("<li>");
+                // Generate HTML for the current node
+                sb.AppendLine("<a href=\"#\" class=\"platinum\">");
+                sb.AppendLine($@"
+                        <img class=""user-rank"" src=""Content/images/user/avatar-2.jpg"" data-toggle=""tooltip"" data-placement=""top"" title=""Platinum"">
+                        <img class=""user-avatar"" src=""Content/images/user/avatar-2.jpg"">
+                        <span class=""user-name"">No Member</span>
+                        <span class=""node-detail"">
+                            <label>Username: No User</label>
+                            <label>Position: NA</label>
+                            <label>Country: NA</label>
+                        </span>
+                    </a>");
+
+
+                // End the list item for the current node
+                sb.AppendLine("</li>");
+
+
+            }
+
+            return sb.ToString();
+        }
     }
 }
