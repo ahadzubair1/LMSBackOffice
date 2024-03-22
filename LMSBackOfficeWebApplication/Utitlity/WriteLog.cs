15e739b83cd8e7c4f790e3c3d9b043149216ae02
Zeeshan Younis <zeeshan@amodevelopers.com>
2024-03-22 12:12:37 +0400

    fix logging issues

--- LMSBackOfficeWebApplication/Utitlity/WriteLog.cs
+++ LMSBackOfficeWebApplication/Utitlity/WriteLog.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ServiceStack;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,7 +10,7 @@ namespace LMSBackOfficeWebApplication.Utitlity
     public class WriteLog
     {
         private static string PageName = HttpContext.Current.Handler.GetType().Name;
-        private static log4net.ILog log = log4net.LogManager.GetLogger(PageName);
+        private static log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().GetDeclaringTypeName());
 
         public static void LogError(Exception ex)
         {
