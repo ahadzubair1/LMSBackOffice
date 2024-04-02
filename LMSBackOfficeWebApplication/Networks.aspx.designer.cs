6756ccce08301c974c73b45a18053ac94d68c73e
Salman Ahmad <salman@vmarketing.com>
2024-04-02 22:49:33 +0400

    Far Buttons

--- LMSBackOfficeWebApplication/Networks.aspx.designer.cs
+++ LMSBackOfficeWebApplication/Networks.aspx.designer.cs
@@ -15,6 +15,15 @@ namespace LMSBackOfficeWebApplication
     {
 
         /// <summary>
+        /// divSerachMessage control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl divSerachMessage;
+
+        /// <summary>
         /// lblSearchMessage control.
         /// </summary>
         /// <remarks>
